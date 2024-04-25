3065cb2f23bbd76edc5279fdc1c8ecab1555d6f4
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-04-25 15:54:44 +0100

    Bat implements IMammal

--- AnimalKingdom/Bat.cs
+++ AnimalKingdom/Bat.cs
@@ -2,8 +2,10 @@ using System;
 
 namespace AnimalKingdom
 {
-    public class Bat : Animal
+    public class Bat : Animal, IMammal
     {
+        public int NumberOfNipples { get => 4}
+
         public override string Sound()
         {
             return base.Sound() + "kii kii";

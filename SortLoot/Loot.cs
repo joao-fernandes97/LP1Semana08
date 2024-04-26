7379578ce93a39ac7873d79969df1be1f20e09c7
joao-fernandes97 <joaofrfernandes.work@gmail.com>
2024-04-26 16:28:40 +0100

    Implement CompareTo

--- SortLoot/Loot.cs
+++ SortLoot/Loot.cs
@@ -1,9 +1,12 @@
+using System;
+using System.Collections.Generic;
+
 namespace SortLoot
 {
     /// <summary>
     /// The Loot class should implement IComparable<Loot>
     /// </summary>
-    public class Loot
+    public class Loot : IComparable<Loot>
     {
         /// <summary>Type of loot.</summary>
         public LootType Kind { get; }
@@ -35,5 +38,27 @@ namespace SortLoot
         /// </returns>
         public override string ToString() =>
             $"[{Kind,15}]\t{Value:f2}\t{Description}";
+
+        /// <summary>
+        /// Implements comparison criteria for Loot
+        /// First tries to order by Loot Value
+        /// If value is the same, orders by Kind of loot alphabetically
+        /// If Kind of loot  is the same, orders by description, alphabetically
+        /// </summary>
+        /// <param name="other">Loot object we're comparing to</param>
+        /// <returns>
+        /// Negative int if instance comes before other
+        /// Positive int if instance comes after other
+        /// 0 if there's no diference
+        /// </returns>
+        public int CompareTo(Loot other)
+        {
+            if(other.Value - Value != 0f)
+                return other.Value - Value > 0 ? 1 : -1;
+            else if (Kind.ToString().CompareTo(other.Kind.ToString()) != 0)
+                return Kind.ToString().CompareTo(other.Kind.ToString());
+            else
+                return Description.CompareTo(other.Description);
+        }
     }
 }
